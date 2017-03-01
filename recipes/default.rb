033329b51ff18caec127e1d050120da033c903a9
AccidentallyTheCable <chris.walker@jukinmedia.com>
2017-03-01 09:58:42 -0800

    DEVOPS-585: Updated cookbook to be more compliant with chef11/12, and removed some funky mapping for host IPs

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,22 +1,22 @@
 #-*- encoding : utf-8 -*-
 
 # Create Directories
-[ node[:elasticsearch][:path][:conf], node[:elasticsearch][:path][:data], node[:elasticsearch][:path][:logs], node[:elasticsearch][:path][:pids] ].each do |path|
+[ node['elasticsearch']['path']['conf'], node['elasticsearch']['path']['data'], node['elasticsearch']['path']['logs'], node['elasticsearch']['path']['pids'] ].each do |path|
   directory path do
-    owner node[:elasticsearch][:user]
-    group node[:elasticsearch][:user]
-    mode 0755
+    owner node['elasticsearch']['user']
+    group node['elasticsearch']['user']
+    mode '0755'
     recursive true
     action :create
   end
 end
 
 template "elasticsearch-env.sh" do
-  path   "#{node[:elasticsearch][:path][:conf]}/elasticsearch-env.sh"
+  path   "#{node['elasticsearch']['path']['conf']}/elasticsearch-env.sh"
   source "elasticsearch-env.sh.erb"
-  owner node[:elasticsearch][:user]
-  group node[:elasticsearch][:user]
-  mode 0755
+  owner node['elasticsearch']['user']
+  group node['elasticsearch']['user']
+  mode '0755'
 end
 
 
@@ -25,7 +25,7 @@ template "elasticsearch.init" do
   path   "/etc/init.d/elasticsearch"
   source "elasticsearch.init.erb"
   owner 'root'
-  mode 0755
+  mode '0755'
 end
 
 # services
@@ -35,15 +35,15 @@ execute "reload-monit" do
 end
 
 # Configration
-instances = node[:opsworks][:layers][:elasticsearch][:instances]
+instances = node['opsworks']['layers']['elasticsearch']['instances']
 hosts = instances.map{ |name, attrs| attrs['private_ip'] }
 
 template "elasticsearch.yml" do
-  path   "#{node[:elasticsearch][:path][:conf]}/elasticsearch.yml"
+  path   "#{node['elasticsearch']['path']['conf']}/elasticsearch.yml"
   source "elasticsearch.yml.erb"
-  owner node[:elasticsearch][:user]
-  group node[:elasticsearch][:user]
-  mode 0755
+  owner node['elasticsearch']['user']
+  group node['elasticsearch']['user']
+  mode '0755'
   variables :hosts => hosts
 end
 
@@ -52,6 +52,6 @@ template "elasticsearch.monitrc" do
   path   "/etc/monit.d/elasticsearch.monitrc"
   source "elasticsearch.monitrc.erb"
   owner 'root'
-  mode 0755
+  mode '0755'
   notifies :run, resources(:execute => "reload-monit")
 end
