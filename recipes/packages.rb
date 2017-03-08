9f0e38c227c0b72043b6e4eed3d8c67d5833c7f1
AccidentallyTheCable <chris.walker@jukinmedia.com>
2017-03-08 11:55:44 -0800

    DEVOPS-585: Added NodeJS and elasticdump installation

--- recipes/packages.rb
+++ recipes/packages.rb
@@ -1,8 +1,33 @@
-#-*- encoding : utf-8 -*-
-package 'curl' do
-  action :install
+directory '/var/local/jukin-install-states' do
+  action :create
 end
 
-package 'tree' do
-  action :install
+bash 'get_noderepo' do
+  code <<-EOH
+    curl --silent --location https://rpm.nodesource.com/setup_7.x | bash -
+    EOH
+  not_if { ::File.exist?('/var/local/jukin-install-states/elasticsearch-nodejs_installed') }
+end
+
+packages = %w(curl tree nodejs)
+
+packages.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+bash 'get_elasticdump' do
+  code <<-EOH
+    npm install elasticdump -g
+    EOH
+  not_if { ::File.exist?('/var/local/jukin-install-states/elasticsearch-nodejs_installed') }
+  notifies :create, 'file[/var/local/jukin-install-states/elasticsearch-nodejs_installed]', :immediate
+end
+
+file '/var/local/jukin-install-states/elasticsearch-nodejs_installed' do
+  user 'root'
+  group 'root'
+  mode '0444'
+  action :nothing
 end
